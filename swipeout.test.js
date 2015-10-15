2a634f7b5eb3ef7118fbdb8c0b0ff9bf0b841049
mitchellvitez <mitchellvitez@gmail.com>
2015-10-14 20:12:53 -0400

    Installed swipeout and genymotion for testing

---
+++ swipeout.test.js
@@ -0,0 +1,13 @@
+var Swipeout = require('react-native-swipeout')
+
+var swipeoutBtns = [
+  {
+    text: 'Button'
+  }
+]
+ 
+<Swipeout right={swipeoutBtns}>
+  <View>
+    <Text>Swipe me left</Text>
+  </View>
+</Swipeout>
